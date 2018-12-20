b3aa53dd8ac8faf2baf1e71f38bac98104a68bbf
Jim Barnes <jimbarnesdeveloper@gmail.com>
2018-12-19 22:32:01 -0500

    Reorganized scss files

--- single-person.php
+++ single-person.php
@@ -5,15 +5,21 @@
         <div class="row">
             <aside class="col-12 col-sm-4 col-md-3">
                 <?php if ( $post->person_portrait ) : ?>
-                    <img src="<?php echo wp_get_attachment_image_src( $post->person_portrait, 'medium' )[0]; ?>" class="img-fluid rounded mb-2">
-                <?php endif; ?>
-                <?php if ( $post->person_title ) : ?>
-                    <p class="text-complimentary text-center "><?php echo $post->person_title; ?></p>
-                <?php endif; ?>
-                <?php if ( $post->person_twitter_profile ) : ?>
-                    <a href="<?php echo $post->person_twitter_profile; ?>" target="_blank" class="btn btn-info btn-block">
-                        <span class="fa fa-twitter mr-2"></span> Follow on Twitter
-                    </a>
+                    <div class="card mb-2">
+                        <img src="<?php echo wp_get_attachment_image_src( $post->person_portrait, 'medium' )[0]; ?>" class="card-img-top">
+                        <?php if ( $post->person_title ) : ?>
+                        <div class="card-body">
+                            <p class="card-text text-center"><?php echo $post->person_title; ?></p>
+                        </div>
+                        <?php endif; ?>
+                        <?php if ( $post->person_twitter_profile ) : ?>
+                        <div class="card-footer p-1">
+                            <a href="<?php echo $post->person_twitter_profile; ?>" target="_blank" class="btn btn-info btn-block">
+                                <span class="fa fa-twitter mr-2"></span> Follow on Twitter
+                            </a>
+                        </div>
+                        <?php endif; ?>
+                    </div>
                 <?php endif; ?>
             </aside>
             <div class="col-12 col-sm-8 col-md-9">
